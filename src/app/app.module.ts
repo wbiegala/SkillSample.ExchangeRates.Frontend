9e3a0487ad32d1a54407dc8a4decae375769bb13
Wojciech Biegała <wojciech.biegala@gmail.com>
2023-10-22 19:04:17 +0200

    Configure material UI and redux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { MaterialLoaderModule } from './material-loader.module';
+import { NgxsModule } from '@ngxs/store';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxsModule.forRoot(),
+    NgxsReduxDevtoolsPluginModule.forRoot({
+      disabled: environment.production,
+    }),
+    MaterialLoaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
